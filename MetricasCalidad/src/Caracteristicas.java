0e2df71a9c0526ef9237c725f764565dc3d41c4e
Unknown <castagna.cristian16@gmail.com>
2018-09-06 16:50:35 -0300

    Caracteristicas
    estas serian las caracteristicas de nuestro modelo, faltaria definir la capacidad de ser operado

---
+++ MetricasCalidad/src/Caracteristicas.java
@@ -0,0 +1,114 @@
+
+public class Caracteristicas {
+	
+	private Boolean encriptacionDatos;
+	private Boolean inicioSesionUsuarios;
+	private Double ordenError;
+	private Integer usoProcesador;
+	private Integer tiempoSinInformarEstado;
+	private Boolean proteccionDatosProcesados;
+	private Boolean logActividades;
+	private Boolean reanudarActividadAnteFalla;
+	private Boolean reanudarEnEstadoAnterior;
+	private Integer	porcentajeComentariosPorMetodo;
+	private Integer complejidadCiclomatica;
+	private Boolean ayduaContextual;
+	private Boolean manualUsuarioIncorporado;
+	//falta capacidad de ser operado
+	private Integer cantSOCompatibles;
+	private Integer cantPasosInstalacion;
+	
+	
+	public Boolean getEncriptacionDatos() {
+		return encriptacionDatos;
+	}
+	public void setEncriptacionDatos(Boolean encriptacionDatos) {
+		this.encriptacionDatos = encriptacionDatos;
+	}
+	public Boolean getInicioSesionUsuarios() {
+		return inicioSesionUsuarios;
+	}
+	public void setInicioSesionUsuarios(Boolean inicioSesionUsuarios) {
+		this.inicioSesionUsuarios = inicioSesionUsuarios;
+	}
+	public Double getOrdenError() {
+		return ordenError;
+	}
+	public void setOrdenError(Double ordenError) {
+		this.ordenError = ordenError;
+	}
+	public Integer getUsoProcesador() {
+		return usoProcesador;
+	}
+	public void setUsoProcesador(Integer usoProcesador) {
+		this.usoProcesador = usoProcesador;
+	}
+	public Integer getTiempoSinInformarEstado() {
+		return tiempoSinInformarEstado;
+	}
+	public void setTiempoSinInformarEstado(Integer tiempoSinInformarEstado) {
+		this.tiempoSinInformarEstado = tiempoSinInformarEstado;
+	}
+	public Boolean getProteccionDatosProcesados() {
+		return proteccionDatosProcesados;
+	}
+	public void setProteccionDatosProcesados(Boolean proteccionDatosProcesados) {
+		this.proteccionDatosProcesados = proteccionDatosProcesados;
+	}
+	public Boolean getLogActividades() {
+		return logActividades;
+	}
+	public void setLogActividades(Boolean logActividades) {
+		this.logActividades = logActividades;
+	}
+	public Boolean getReanudarActividadAnteFalla() {
+		return reanudarActividadAnteFalla;
+	}
+	public void setReanudarActividadAnteFalla(Boolean reanudarActividadAnteFalla) {
+		this.reanudarActividadAnteFalla = reanudarActividadAnteFalla;
+	}
+	public Boolean getReanudarEnEstadoAnterior() {
+		return reanudarEnEstadoAnterior;
+	}
+	public void setReanudarEnEstadoAnterior(Boolean reanudarEnEstadoAnterior) {
+		this.reanudarEnEstadoAnterior = reanudarEnEstadoAnterior;
+	}
+	public Integer getPorcentajeComentariosPorMetodo() {
+		return porcentajeComentariosPorMetodo;
+	}
+	public void setPorcentajeComentariosPorMetodo(Integer porcentajeComentariosPorMetodo) {
+		this.porcentajeComentariosPorMetodo = porcentajeComentariosPorMetodo;
+	}
+	public Integer getComplejidadCiclomatica() {
+		return complejidadCiclomatica;
+	}
+	public void setComplejidadCiclomatica(Integer complejidadCiclomatica) {
+		this.complejidadCiclomatica = complejidadCiclomatica;
+	}
+	public Boolean getAyduaContextual() {
+		return ayduaContextual;
+	}
+	public void setAyduaContextual(Boolean ayduaContextual) {
+		this.ayduaContextual = ayduaContextual;
+	}
+	public Boolean getManualUsuarioIncorporado() {
+		return manualUsuarioIncorporado;
+	}
+	public void setManualUsuarioIncorporado(Boolean manualUsuarioIncorporado) {
+		this.manualUsuarioIncorporado = manualUsuarioIncorporado;
+	}
+	public Integer getCantSOCompatibles() {
+		return cantSOCompatibles;
+	}
+	public void setCantSOCompatibles(Integer cantSOCompatibles) {
+		this.cantSOCompatibles = cantSOCompatibles;
+	}
+	public Integer getCantPasosInstalacion() {
+		return cantPasosInstalacion;
+	}
+	public void setCantPasosInstalacion(Integer cantPasosInstalacion) {
+		this.cantPasosInstalacion = cantPasosInstalacion;
+	}
+	
+
+}
